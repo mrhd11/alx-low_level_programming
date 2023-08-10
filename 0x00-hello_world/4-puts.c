b6037563d025096c6fa895143be75180baac209b
mrhd11 <ahmedfather555@gmail.com>
2023-08-11 00:51:33 +0300

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-int main (void)
+/**
+ * main -entry point
+ * should retun0
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0); 
+	return (0);
 }
