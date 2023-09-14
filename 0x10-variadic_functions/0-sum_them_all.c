8a110ac92778d6a9064b44a50c5b85215ef5d396
mrhd11 <ahmedfather555@gmail.com>
2023-09-15 00:20:56 +0300

    udpate

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -16,9 +16,13 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_start(args, n);
 
-	int sum = 0;
+	int sum;
 
-	for (int i = 0; i < n; i++)
+	sum = 0;
+	
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
 	{
 		result += va_arg(args, int);
 	}
